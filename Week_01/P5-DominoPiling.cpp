cb647af31319d7b95f2fb868fadefdaeba16e2dd
Akash Singh <developer.akash8@gmail.com>
2023-06-02 00:07:25 +0530

    Solved - 50A Domino piling

---
+++ Week_01/P5-DominoPiling.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int m, n;
+    cin >> m >> n;
+
+    int totalSquares = m * n;
+
+    cout << totalSquares / 2;
+
+    return 0;
+}
