3cf07780e58ecb139939db61af05c60ce94e9b7a
Akash Singh <developer.akash8@gmail.com>
2023-06-05 23:27:08 +0530

    Solved - 791A Bear & Big Brother

---
+++ Week_01/P13-Bear&BigBro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a, b, years = 0;
+    cin >> a >> b;
+
+    while (a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        years += 1;
+    }
+
+    cout << years;
+
+    return 0;
+}
