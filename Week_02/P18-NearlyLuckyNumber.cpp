012697429d1c96102d43ea9d043f7a7a8a1e18c4
Akash Singh <developer.akash8@gmail.com>
2023-06-08 23:17:37 +0530

    Solved - 110A Nearly Lucky Number

---
+++ Week_02/P18-NearlyLuckyNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    long long luckyCount = 0;
+    cin >> n;
+
+    while (n)
+    {
+        if (n % 10 == 4 || n % 10 == 7)
+            luckyCount += 1;
+        n /= 10;
+    }
+
+    if (luckyCount == 4 || luckyCount == 7)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
